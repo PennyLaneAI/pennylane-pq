198b65a534a4be6d91228d5ab47852fd03df81b8
Christian Gogolin <git@cgogolin.de>
2018-10-18 11:31:30 +0200

    implemented changes in naming convention, addressing #7

--- openqml_pq/pqops.py
+++ openqml_pq/pqops.py
@@ -83,11 +83,11 @@ class Rot(BasicProjectQGate):
     ProjectQ does not currently have an arbitrary single qubit rotation gate, so we provide a class that return a suitable combination of rotation gates assembled into a single gate from the constructor of this class.
     """
     def __new__(*par):
-        gate1 = pq.ops.Rz(par[1])
-        gate2 = pq.ops.Ry(par[2])
-        gate3 = pq.ops.Rz(par[3])
+        operation1 = pq.ops.Rz(par[1])
+        operation2 = pq.ops.Ry(par[2])
+        operation3 = pq.ops.Rz(par[3])
         rot_gate = BasicProjectQGate(par[0].__name__)
-        rot_gate.matrix = np.dot(gate3.matrix, gate2.matrix, gate1.matrix) #todo: fix depending on how https://github.com/XanaduAI/openqml/issues/87 is resolved.
+        rot_gate.matrix = np.dot(operation3.matrix, operation2.matrix, operation1.matrix)
         return rot_gate
 
 class QubitUnitary(BasicProjectQGate): # pylint: disable=too-few-public-methods
