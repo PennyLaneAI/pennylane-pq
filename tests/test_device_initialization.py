ea33205d621b74489cc4ae85b84105e2dd9a6694
Josh Izaac <josh146@gmail.com>
2020-04-23 21:13:19 +0930

    added

--- tests/test_device_initialization.py
+++ tests/test_device_initialization.py
@@ -49,6 +49,7 @@ class DeviceInitialization(BaseTest):
     def test_shots(self):
         if self.args.device == 'ibm' or self.args.device == 'all':
             shots = 5
+            print(token)
             dev1 = ProjectQIBMBackend(wires=self.num_subsystems, shots=shots, use_hardware=False, token=token, verbose=True)
             self.assertEqual(shots, dev1.shots)
 
