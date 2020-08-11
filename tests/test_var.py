34730266e14d4cccd36d38eb69f5866c792fba42
Josh Izaac <josh146@gmail.com>
2020-08-11 21:47:24 +0930

    fix

--- tests/test_var.py
+++ tests/test_var.py
@@ -26,7 +26,6 @@ from pennylane_pq.devices import ProjectQSimulator, ProjectQClassicalSimulator, 
 import os
 
 token = os.getenv("IBMQX_TOKEN")
-print("Token here hello", token)
 
 
 @pytest.fixture
